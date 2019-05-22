e84d2928773e525114df43e4f51f90a56cc31a7c
ralonsodeniz <ralonsodeniz@gmail.com>
2019-05-22 22:40:20 +0200

    Updates

---
+++ precache-manifest.fb6d0ab2820c4b0cf8ddb549fb8d8f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7b7aa5635c85e513a23",
+    "url": "/deployrobofriendsredux/static/css/main.46069a59.chunk.css"
+  },
+  {
+    "revision": "d7b7aa5635c85e513a23",
+    "url": "/deployrobofriendsredux/static/js/main.5b3e61d0.chunk.js"
+  },
+  {
+    "revision": "24264dab7d55b5dfc5c8",
+    "url": "/deployrobofriendsredux/static/js/runtime~main.36934f42.js"
+  },
+  {
+    "revision": "4af5950e11fb1c2d174f",
+    "url": "/deployrobofriendsredux/static/css/2.ed124330.chunk.css"
+  },
+  {
+    "revision": "4af5950e11fb1c2d174f",
+    "url": "/deployrobofriendsredux/static/js/2.3a9782a3.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/deployrobofriendsredux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e1b42b23656e1974cfa4c6b83e540eac",
+    "url": "/deployrobofriendsredux/index.html"
+  }
+];
