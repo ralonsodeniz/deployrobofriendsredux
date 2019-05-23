ddf54e4297067885ac9ea5f35d218d5f298ab07f
ralonsodeniz <ralonsodeniz@gmail.com>
2019-05-23 13:44:49 +0200

    added serviceWorker

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 // to install redux we do npm install redux
 // npm install react-redux to connect react with redux store
